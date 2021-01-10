59c1123a01eeb05c912856a00d1a3acdab19eee2
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-01-10 19:48:26 +0800

    Feat: Duration function

--- src/App.js
+++ src/App.js
@@ -10,22 +10,45 @@ function App() {
   const [inputTitle, setInputTitle] = useState("");
   const [inputStart, setInputStart] = useState("");
   const [inputEnd, setInputEnd] = useState("");
+  const [inputDur, setInputDur] = useState("30");
   const [inputDesc, setInputDesc] = useState("");
   const [todos, setTodos] = useState([]);
   const [sortedTodos, setSortedTodos] = useState([]);
+  const [durationText, setDurationText] = useState(false);
 
   const popupHandler = () => {
     setPopup(!popup);
   };
   const createTodoHandler = (e) => {
     e.preventDefault();
+    let inputEndDur;
+    if (durationText) {
+      const hours = inputStart.substring(0, 2);
+      const mins = inputStart.substring(2, 4);
+      let hoursNum = Number(hours);
+      let minsNum = Number(mins);
+      minsNum += Number(inputDur);
+      let offset = Math.floor(minsNum / 60);
+      minsNum %= 60;
+      hoursNum += offset;
+      setInputEnd(
+        hoursNum.toString().padStart(2, "0") +
+          minsNum.toString().padStart(2, "0")
+      );
+      inputEndDur =
+        hoursNum.toString().padStart(2, "0") +
+        minsNum.toString().padStart(2, "0");
+      console.log(hoursNum);
+      console.log(minsNum);
+    }
+
     setTodos([
       ...todos,
       {
         title: inputTitle,
         desc: inputDesc ? inputDesc : "Nothing special ...",
         start: inputStart,
-        end: inputEnd,
+        end: durationText ? inputEndDur : inputEnd,
       },
     ]);
     setInputTitle("");
@@ -40,9 +63,12 @@ function App() {
       return Number(a.start) - Number(b.start);
     }
     setSortedTodos(todos.sort(compare));
-    setCurrentTime();
+    // setCurrentTime();
   }, [todos]);
 
+  useEffect(() => {
+    setCurrentTime();
+  }, [popup]);
   const setCurrentTime = () => {
     const duration = 30;
     const time = new Date();
@@ -75,8 +101,12 @@ function App() {
           setInputStart={setInputStart}
           inputEnd={inputEnd}
           setInputEnd={setInputEnd}
+          inputDur={inputDur}
+          setInputDur={setInputDur}
           inputDesc={inputDesc}
           setInputDesc={setInputDesc}
+          durationText={durationText}
+          setDurationText={setDurationText}
         />
         <div className="bottom"></div>
       </div>
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,41 +1,109 @@
-import React from 'react';
-import Button from './Button';
+import React, { useState } from "react";
+import Button from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
 
-const Popup = ({createTodoHandler, popup, popupHandler, inputTitle, setInputTitle, inputDesc, setInputDesc, inputStart, setInputStart, inputEnd, setInputEnd}) => {
+const Popup = ({
+  createTodoHandler,
+  popup,
+  popupHandler,
+  inputTitle,
+  setInputTitle,
+  inputDesc,
+  setInputDesc,
+  inputStart,
+  setInputStart,
+  inputEnd,
+  setInputEnd,
+  inputDur,
+  setInputDur,
+  durationText,
+  setDurationText,
+}) => {
   const inputTitleHandler = (e) => {
-    setInputTitle(e.target.value);       
-  }
+    setInputTitle(e.target.value);
+  };
   const inputDescHandler = (e) => {
-    setInputDesc(e.target.value);       
-  }
+    setInputDesc(e.target.value);
+  };
   const inputStartHandler = (e) => {
-    setInputStart(e.target.value);       
-  }
+    setInputStart(e.target.value);
+  };
   const inputEndHandler = (e) => {
-    setInputEnd(e.target.value);       
-  }
+    setInputEnd(e.target.value);
+  };
 
-  return (    
+  const inputDurHandler = (e) => {
+    setInputDur(e.target.value);
+  };
+
+  return (
     <div className="popup">
       <form className={popup ? "popup-content active" : "popup-content"}>
-        <textarea onChange={inputTitleHandler} className="input-title" placeholder="Sleep all day? Fail to wake ..." value={inputTitle} rows="2"></textarea>
+        <textarea
+          onChange={inputTitleHandler}
+          className="input-title"
+          placeholder="Sleep all day? Fail to wake ..."
+          value={inputTitle}
+          rows="2"
+        ></textarea>
         <div className="field-wrap">
           <div className="field-start">
             <p className="label-start">When to start?</p>
-            <input onChange={inputStartHandler} className="input-start" placeholder="0900" value={inputStart} type="text" maxLength="4"/>
+            <input
+              onChange={inputStartHandler}
+              className="input-start"
+              placeholder="0900"
+              value={inputStart}
+              type="text"
+              maxLength="4"
+            />
           </div>
           <div className="field-end">
-            <p className="label-end">When to end?</p>
-            <input onChange={inputEndHandler} className="input-end" placeholder="1000" value={inputEnd} type="text" maxLength="4"/>
+            <p className="label-end">
+              {durationText ? "How long?" : "When to end?"}
+            </p>
+            <div className="wrapper">
+              <div
+                className="button-duration"
+                onClick={() => setDurationText(!durationText)}
+              >
+                <FontAwesomeIcon className="icon" icon={faClock} />
+              </div>
+              <input
+                onChange={durationText ? inputDurHandler : inputEndHandler}
+                className={durationText ? "input-dur" : "input-end"}
+                placeholder="1000"
+                value={durationText ? inputDur : inputEnd}
+                type="text"
+                maxLength="4"
+              />
+              <div
+                className={
+                  durationText ? "duration-unit active" : "duration-unit"
+                }
+              >
+                mins
+              </div>
+            </div>
           </div>
         </div>
         <p className="label-desc">Descriptions</p>
-        <textarea onChange={inputDescHandler} className="input-desc" placeholder="Apple, Banana, Carrot ..." value={inputDesc} rows="5"></textarea>
+        <textarea
+          onChange={inputDescHandler}
+          className="input-desc"
+          placeholder="Apple, Banana, Carrot ..."
+          value={inputDesc}
+          rows="5"
+        ></textarea>
         <Button action={createTodoHandler} text="Create" />
       </form>
-      <div onClick={popupHandler} className={popup ? "popup-bg active" : "popup-bg"}></div>
+      <div
+        onClick={popupHandler}
+        className={popup ? "popup-bg active" : "popup-bg"}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
 export default Popup;
